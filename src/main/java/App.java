7fbe9026d6455a61701c44e1ba89a97e2cadfce2
Iurii Varankin <varan555@yandex.ru>
2020-05-23 01:18:50 +0300

    Final commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,26 +1,36 @@
+import sweet.Juice;
+
 public class App {
 
 
     public static void main(String[] args) {
         Box box = new Box();
-        box.creatPresent();
         System.out.println("Список сладостей:");
         box.showAllSweet();
         System.out.println("-----------------------------------");
-        box.deleteByID( 2);
 
+        box.deleteByID( 2);
         System.out.println("Список сладостей после удаления третьего подарка:");
         box.showAllSweet();
         System.out.println("-----------------------------------");
+
         box.deleteLast();
         System.out.println("Список сладостей после удаления последнего подарка:");
         box.showAllSweet();
         System.out.println("-----------------------------------");
+
+        box.add(new Juice(1.5, "Кола", 1.5, 2.8));
+        System.out.println("Список сладостей после добавления сладости:");
+        box.showAllSweet();
+        System.out.println("-----------------------------------");
+
         System.out.println("Общий вес подарка:  " + box.showWeight());
         System.out.println("-----------------------------------");
         System.out.println("Общая стоимость подарка:  " +box.showPrice());
-        box.reduceWeight(1.00);
-        box.reducePrice(3);
+        System.out.println("-----------------------------------");
+
+        box.reduceWeight(2.00);
+        box.reducePrice(5);
 
     }
 }
